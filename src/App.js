59a828d8f8ccf06a25da9826fce55b8f37461310
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-06-10 20:17:25 +0300

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+// Components
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
+import NavBar from "./components/NavBar/NavBar";
+import Search from "./components/Search/Search";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <NavBar />
+      <div className="App">
+        <Search />
+        <div>
+          <Filters />
+          <Cards />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="container mx-auto mb-10">
+      <div className="border-b w-full inline-block border-blue-400 py-8 bg-white flex justify-between">
+        <h1 className="text-2xl font-semibold text-gray-800">
+          Rick & Morty <span className="text-blue-500">Wiki</span>
+        </h1>
+        <div className="flex gap-2">
+          <a
+            href="https://rickandmortyapi.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              className="transition duration-300 ease bg-white hover:bg-slate-100 px-10
+         group-hover:text-white p-2 rounded-md"
+            >
+              API
+            </button>
+          </a>
+          <a
+            href="https://github.com/thenesern"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button
+              className="transition duration-300 ease bg-white hover:bg-slate-100 px-10
+         group-hover:text-white p-2 rounded-md"
+            >
+              Author
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
