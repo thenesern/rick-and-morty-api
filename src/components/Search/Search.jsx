88b31cbb989d0efa2a9d427a35e01b1986b8ca99
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-06-10 20:28:08 +0300

    Added Search Component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="w-full flex justify-center">
+      <input
+        className="px-5 py-3 bg-gray-100 w-2/5 outline-none rounded-sm"
+        type="text"
+        placeholder="Search for a character"
+      />
+    </div>
+  );
 };
 
 export default Search;
