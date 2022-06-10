335dfdf028f0a02765e8a2efce6803c71b6cec13
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-06-10 20:51:14 +0300

    Added Data Fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState, useEffect, useLayoutEffect } from "react";
 // Components
 import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 import NavBar from "./components/NavBar/NavBar";
 import Search from "./components/Search/Search";
+// Axios
+import axios from "axios";
 
 function App() {
+  const [episodes, setEpisodes] = useState(null);
+  const [page, setPage] = useState(1);
+
+  // Fetching episodes from the API via axios
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/episode/?page=${page}`)
+      .then((result) => setEpisodes(result));
+  }, []);
+  console.log(episodes);
+
   return (
     <>
       <NavBar />
